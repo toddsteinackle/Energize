99485c79b07c86110905e494b449513325daaf17
Todd Steinackle <todd@noquarterarcade.com>
2011-04-15 18:44:05 -0400

    Added iPad main menu buttons and animation graphics

--- MainMenuView.h
+++ MainMenuView.h
@@ -6,10 +6,15 @@
 @class CubeStormAppDelegate;
 
 @interface MainMenuView : UIView {
-    IBOutlet UIView* subview;
+    IBOutlet UIView *subview;
     CubeStormAppDelegate *appDelegate;
-    IBOutlet UIButton* leaderboardButton;
+    IBOutlet UIButton *leaderboardButton;
+    IBOutlet UIImageView *purpleCube;
+    IBOutlet UIImageView *redCube;
+    IBOutlet UIImageView *purpleCube2;
+    IBOutlet UIImageView *redCube2;
 }
+
 
 - (IBAction)doGraphicsTest;
 - (IBAction)doMenuTest;
--- MainMenuView.m
+++ MainMenuView.m
@@ -24,6 +24,80 @@
         if (!appDelegate.gameCenterAvailable) {
             leaderboardButton.hidden = TRUE;
         }
+
+        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+            // set up our UIImage with a group or array of images to animate
+            purpleCube.animationImages = [NSArray arrayWithObjects:
+                                            [UIImage imageNamed:@"iPadPurple0008.png"],
+                                            [UIImage imageNamed:@"iPadPurple0009.png"],
+                                            [UIImage imageNamed:@"iPadPurple0010.png"],
+                                            [UIImage imageNamed:@"iPadPurple0011.png"],
+                                            [UIImage imageNamed:@"iPadPurple0012.png"],
+                                            [UIImage imageNamed:@"iPadPurple0013.png"],
+                                            [UIImage imageNamed:@"iPadPurple0014.png"],
+                                            [UIImage imageNamed:@"iPadPurple0015.png"],
+                                            [UIImage imageNamed:@"iPadPurple0016.png"],
+                                            [UIImage imageNamed:@"iPadPurple0005.png"],
+                                            [UIImage imageNamed:@"iPadPurple0006.png"],
+                                            [UIImage imageNamed:@"iPadPurple0007.png"],
+                                            nil];
+            purpleCube.animationDuration = .6;
+            [purpleCube startAnimating];
+
+            redCube.animationImages = [NSArray arrayWithObjects:
+                                            [UIImage imageNamed:@"iPadRed0016.png"],
+                                            [UIImage imageNamed:@"iPadRed0015.png"],
+                                            [UIImage imageNamed:@"iPadRed0014.png"],
+                                            [UIImage imageNamed:@"iPadRed0013.png"],
+                                            [UIImage imageNamed:@"iPadRed0012.png"],
+                                            [UIImage imageNamed:@"iPadRed0011.png"],
+                                            [UIImage imageNamed:@"iPadRed0010.png"],
+                                            [UIImage imageNamed:@"iPadRed0009.png"],
+                                            [UIImage imageNamed:@"iPadRed0008.png"],
+                                            [UIImage imageNamed:@"iPadRed0007.png"],
+                                            [UIImage imageNamed:@"iPadRed0006.png"],
+                                            [UIImage imageNamed:@"iPadRed0005.png"],
+                                            nil];
+            redCube.animationDuration = .6;
+            [redCube startAnimating];
+
+            purpleCube2.animationImages = [NSArray arrayWithObjects:
+                                            [UIImage imageNamed:@"iPadPurple0008.png"],
+                                            [UIImage imageNamed:@"iPadPurple0009.png"],
+                                            [UIImage imageNamed:@"iPadPurple0010.png"],
+                                            [UIImage imageNamed:@"iPadPurple0011.png"],
+                                            [UIImage imageNamed:@"iPadPurple0012.png"],
+                                            [UIImage imageNamed:@"iPadPurple0013.png"],
+                                            [UIImage imageNamed:@"iPadPurple0014.png"],
+                                            [UIImage imageNamed:@"iPadPurple0015.png"],
+                                            [UIImage imageNamed:@"iPadPurple0016.png"],
+                                            [UIImage imageNamed:@"iPadPurple0005.png"],
+                                            [UIImage imageNamed:@"iPadPurple0006.png"],
+                                            [UIImage imageNamed:@"iPadPurple0007.png"],
+                                            nil];
+            purpleCube2.animationDuration = .6;
+            [purpleCube2 startAnimating];
+
+            redCube2.animationImages = [NSArray arrayWithObjects:
+                                            [UIImage imageNamed:@"iPadRed0016.png"],
+                                            [UIImage imageNamed:@"iPadRed0015.png"],
+                                            [UIImage imageNamed:@"iPadRed0014.png"],
+                                            [UIImage imageNamed:@"iPadRed0013.png"],
+                                            [UIImage imageNamed:@"iPadRed0012.png"],
+                                            [UIImage imageNamed:@"iPadRed0011.png"],
+                                            [UIImage imageNamed:@"iPadRed0010.png"],
+                                            [UIImage imageNamed:@"iPadRed0009.png"],
+                                            [UIImage imageNamed:@"iPadRed0008.png"],
+                                            [UIImage imageNamed:@"iPadRed0007.png"],
+                                            [UIImage imageNamed:@"iPadRed0006.png"],
+                                            [UIImage imageNamed:@"iPadRed0005.png"],
+                                            nil];
+            redCube2.animationDuration = .6;
+            [redCube2 startAnimating];
+        } else {
+            // iphone cubes
+        }
+
     }
     return self;
 }
